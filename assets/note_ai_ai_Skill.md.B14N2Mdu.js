2ca2890ccfa710c405aa136b4b8142caeb691667
coder-lzh <657083979@qq.com>
2026-04-26 14:34:35 +0000

    Update gh-pages to output generated at c4bfbc9

---
+++ assets/note_ai_ai_Skill.md.B14N2Mdu.js
@@ -0,0 +1,15 @@
+import{_ as a,c as n,aU as l,a as e}from"./chunks/framework.DIXiA2XH.js";const m=JSON.parse('{"title":"Skill","description":"","frontmatter":{},"headers":[],"relativePath":"note/ai/ai/Skill.md","filePath":"note/ai/ai/Skill.md","lastUpdated":1777213909000}'),p={name:"note/ai/ai/Skill.md"};function i(r,s,t,c,b,o){return e(),n("div",null,[...s[0]||(s[0]=[l(`<h1 id="skill" tabindex="-1">Skill <a class="header-anchor" href="#skill" aria-label="Permalink to &quot;Skill&quot;">​</a></h1><h2 id="skill打包提示词" tabindex="-1">Skill打包提示词 <a class="header-anchor" href="#skill打包提示词" aria-label="Permalink to &quot;Skill打包提示词&quot;">​</a></h2><div class="language-text vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">text</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>我们刚才的对话已经磨合出了完整的工作流程和输出标准。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>请现在将这个过程整理成一个标准的Agent Skill，要求如下:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>1.创建完整的Skil文件夹结构</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>2.SKILL.md写清楚:Skill职责、触发场景、执行步骤、输出标准</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>3.references放入我们确认过的所有格式要求和内容标准</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>4.可自动化的步骤写入scripts</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>5.assets放入需要复用的模板文件</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>输出一个我可以直接安装使用的Skil文件夹</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br></div></div>`,3)])])}const u=a(p,[["render",i]]);export{m as __pageData,u as default};
